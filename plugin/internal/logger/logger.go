d70a96694b4f273faa544012a8aa4bf45eb545e5
Heiru Wu <heiru.wu@instill.tech>
2023-05-11 01:07:52 +0800

    chore(logger): update logger output target

--- plugin/internal/logger/logger.go
+++ plugin/internal/logger/logger.go
@@ -1,18 +1,50 @@
 package logger
 
 import (
+	"os"
 	"sync"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var logger *zap.Logger
 var once sync.Once
 
+// GetZapLogger returns an instance of zap logger
 func GetZapLogger() (*zap.Logger, error) {
 	var err error
 	once.Do(func() {
-		logger, err = zap.NewProduction()
+		// info level enabler
+		infoLevel := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+			return level == zapcore.InfoLevel
+		})
+
+		// error and fatal level enabler
+		errorFatalLevel := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+			return level == zapcore.WarnLevel || level == zapcore.ErrorLevel || level == zapcore.FatalLevel
+		})
+
+		// write syncers
+		stdoutSyncer := zapcore.Lock(os.Stdout)
+		stderrSyncer := zapcore.Lock(os.Stderr)
+
+		// tee core
+		core := zapcore.NewTee(
+			zapcore.NewCore(
+				zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+				stdoutSyncer,
+				infoLevel,
+			),
+			zapcore.NewCore(
+				zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+				stderrSyncer,
+				errorFatalLevel,
+			),
+		)
+
+		// finally construct the logger with the tee core
+		logger = zap.New(core)
 	})
 
 	return logger, err
