a4829046f3d296f3e00cec1afea4a084ee5f52b5
Chang, Hui-Tang <huitang.chang@instill.tech>
2023-09-21 22:55:05 +0800

    feat(plugin): add multi_auth plugin to support `api_token` authentication and basic_auth (#87)
    Because
    
    - we need to support both `jwt` and `api_token` authentication
    
    This commit
    
    - add multi_auth plugin to support api_token authentication

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,8 @@ RUN apk --no-cache --virtual .build-deps add tar make gcc musl-dev binutils-gold
 
 WORKDIR /${SERVICE_NAME}
 
-COPY plugin plugin
+COPY grpc_proxy_plugin grpc_proxy_plugin
+COPY multi_auth_plugin multi_auth_plugin
 
 ARG TARGETARCH
 ARG BUILDARCH
@@ -18,13 +19,25 @@ RUN if [[ "$BUILDARCH" = "amd64" && "$TARGETARCH" = "arm64" ]] ; \
     curl -sL http://musl.cc/aarch64-linux-musl-cross.tgz | \
     tar zx && \
     export PATH="$PATH:/${SERVICE_NAME}/aarch64-linux-musl-cross/bin" && \
-    cd plugin && go mod download && \
+    cd grpc_proxy_plugin && go mod download && \
     CGO_ENABLED=1 ARCH=$TARGETARCH GOARCH=$TARGETARCH GOHOSTARCH=$BUILDARCH \
     CC=aarch64-linux-musl-gcc EXTRA_LDFLAGS='-extld=aarch64-linux-musl-gcc' \
     go build -buildmode=plugin -buildvcs=false -o grpc-proxy.so ./client; \
     else \
-    cd plugin && go mod download && \
+    cd grpc_proxy_plugin && go mod download && \
     CGO_ENABLED=1 go build -buildmode=plugin -buildvcs=false -o grpc-proxy.so ./client; fi
+RUN if [[ "$BUILDARCH" = "amd64" && "$TARGETARCH" = "arm64" ]] ; \
+    then \
+    curl -sL http://musl.cc/aarch64-linux-musl-cross.tgz | \
+    tar zx && \
+    export PATH="$PATH:/${SERVICE_NAME}/aarch64-linux-musl-cross/bin" && \
+    cd multi_auth_plugin && go mod download && \
+    CGO_ENABLED=1 ARCH=$TARGETARCH GOARCH=$TARGETARCH GOHOSTARCH=$BUILDARCH \
+    CC=aarch64-linux-musl-gcc EXTRA_LDFLAGS='-extld=aarch64-linux-musl-gcc' \
+    go build -buildmode=plugin -o multi-auth.so ./server; \
+    else \
+    cd multi_auth_plugin && go mod download && \
+    CGO_ENABLED=1 go build -buildmode=plugin -o multi-auth.so ./server; fi
 
 RUN git clone -b v2.0.12 https://github.com/lestrrat-go/jwx.git && cd jwx && make jwx && cd .. && rm -r jwx
 
@@ -36,7 +49,10 @@ ARG SERVICE_NAME
 
 WORKDIR /${SERVICE_NAME}
 
-COPY --from=build --chown=krakend:nogroup /${SERVICE_NAME}/plugin /usr/local/lib/krakend/plugin
+RUN mkdir -p /usr/local/lib/krakend/plugin && chmod 777 /usr/local/lib/krakend/plugin
+
+COPY --from=build --chown=krakend:nogroup /${SERVICE_NAME}/grpc_proxy_plugin/grpc-proxy.so /usr/local/lib/krakend/plugin
+COPY --from=build --chown=krakend:nogroup /${SERVICE_NAME}/multi_auth_plugin/multi-auth.so /usr/local/lib/krakend/plugin
 COPY --from=build --chown=krakend:nogroup /go/bin/jwx /go/bin/jwx
 RUN mkdir -p /instill && chmod 777 /instill
 
---
+++ multi_auth_plugin/server/main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+// pluginName is the plugin name
+var pluginName = "multi-auth"
+
+// HandlerRegisterer is the symbol the plugin loader will try to load. It must implement the Registerer interface
+var HandlerRegisterer = registerer(pluginName)
+
+type registerer string
+
+func (r registerer) RegisterHandlers(f func(
+	name string,
+	handler func(context.Context, map[string]interface{}, http.Handler) (http.Handler, error),
+)) {
+	f(string(r), r.registerHandlers)
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	AccessToken struct {
+		Sub string `json:"sub"`
+	} `json:"access_token"`
+}
+
+type ValidateTokenResp struct {
+	UserUid string `json:"user_uid"`
+}
+
+func writeStatusUnauthorized(req *http.Request, w http.ResponseWriter) {
+
+	if req.ProtoMajor == 2 && strings.Contains(req.Header.Get("Content-Type"), "application/grpc") {
+		w.Header().Set("Content-Type", "application/grpc")
+		w.Header().Set("Trailer", "Grpc-Status, Grpc-Message")
+		w.Header().Set("Grpc-Status", "16") // UNAUTHENTICATED
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Header().Set("Content-Type", "application/json")
+	}
+}
+
+func (r registerer) registerHandlers(ctx context.Context, extra map[string]interface{}, h http.Handler) (http.Handler, error) {
+
+	logger, _ := GetZapLogger()
+	defer func() {
+		// can't handle the error due to https://github.com/uber-go/zap/issues/880
+		_ = logger.Sync()
+	}()
+	grpc_zap.ReplaceGrpcLoggerV2(logger)
+
+	config, ok := extra[pluginName].(map[string]interface{})
+	if !ok {
+		return h, errors.New("configuration not found")
+	}
+
+	return h2c.NewHandler(
+		http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			authorization := req.Header.Get("Authorization")
+
+			if req.URL.String() == "/__health" {
+				h.ServeHTTP(w, req)
+			} else if req.URL.String() == "/base/v1alpha/validate_token" {
+				h.ServeHTTP(w, req)
+			} else if req.URL.String() == "/base/v1alpha/auth/login" {
+				h.ServeHTTP(w, req)
+			} else if strings.HasPrefix(authorization, "Basic ") || strings.HasPrefix(authorization, "basic ") {
+				basicAuth := strings.Split(authorization, " ")[1]
+
+				basicAuthDecoded, err := base64.StdEncoding.DecodeString(basicAuth)
+				if err != nil {
+					writeStatusUnauthorized(req, w)
+					return
+				}
+
+				loginRequest := LoginRequest{
+					Username: strings.Split(string(basicAuthDecoded), ":")[0],
+					Password: strings.Split(string(basicAuthDecoded), ":")[1],
+				}
+				loginRequestJson, err := json.Marshal(loginRequest)
+				if err != nil {
+					writeStatusUnauthorized(req, w)
+					return
+				}
+
+				loginReq, err := http.NewRequest("POST", config["token_issuer_endpoint"].(string), bytes.NewBuffer(loginRequestJson))
+				if err != nil {
+					writeStatusUnauthorized(req, w)
+					return
+				}
+
+				client := &http.Client{}
+				loginResponseJson, err := client.Do(loginReq)
+				if err != nil {
+					writeStatusUnauthorized(req, w)
+					return
+				}
+
+				defer loginResponseJson.Body.Close()
+
+				respBody, err := ioutil.ReadAll(loginResponseJson.Body)
+				if err != nil {
+					writeStatusUnauthorized(req, w)
+					return
+				}
+				var loginResponse LoginResponse
+
+				err = json.Unmarshal(respBody, &loginResponse)
+
+				if err != nil {
+					writeStatusUnauthorized(req, w)
+					return
+				}
+
+				req.Header.Set("jwt-sub", loginResponse.AccessToken.Sub)
+				h.ServeHTTP(w, req)
+
+			} else if strings.HasPrefix(authorization, "Bearer instill_sk_") || strings.HasPrefix(authorization, "bearer instill_sk_") {
+				reqValidate, err := http.NewRequest("POST", config["token_validation_endpoint"].(string), nil)
+
+				if err != nil {
+					writeStatusUnauthorized(req, w)
+					return
+				}
+				reqValidate.Header = req.Header
+				resValidate, err := http.DefaultClient.Do(reqValidate)
+
+				if err != nil {
+					writeStatusUnauthorized(req, w)
+					return
+				}
+				defer resValidate.Body.Close()
+				if resValidate.StatusCode == 200 {
+					resValidateStruct := &ValidateTokenResp{}
+					json.NewDecoder(resValidate.Body).Decode(resValidateStruct)
+					req.Header.Set("jwt-sub", resValidateStruct.UserUid)
+					h.ServeHTTP(w, req)
+				} else {
+					writeStatusUnauthorized(req, w)
+				}
+
+			} else {
+				req.URL.Path = "/internal" + req.URL.Path
+				h.ServeHTTP(w, req)
+			}
+		}),
+		&http2.Server{},
+	), nil
+
+}
+
+func init() {
+	fmt.Printf("Plugin: router handler \"%s\" loaded!!!\n", HandlerRegisterer)
+}
+
+func main() {}
