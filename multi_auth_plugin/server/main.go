bdeebc0c4ffd21fb7e1dacd4d72521df24ceaf10
Chang, Hui-Tang <huitang.chang@instill.tech>
2023-12-05 02:36:22 +0800

    feat(core,vdp): upgrade endpoints version from v1alpha to v1beta

--- multi_auth_plugin/server/main.go
+++ multi_auth_plugin/server/main.go
@@ -82,9 +82,9 @@ func (r registerer) registerHandlers(ctx context.Context, extra map[string]inter
 
 		if req.URL.String() == "/__health" {
 			h.ServeHTTP(w, req)
-		} else if req.URL.String() == "/core/v1alpha/validate_token" {
+		} else if req.URL.String() == "/core/v1beta/validate_token" {
 			h.ServeHTTP(w, req)
-		} else if req.URL.String() == "/core/v1alpha/auth/login" {
+		} else if req.URL.String() == "/core/v1beta/auth/login" {
 			h.ServeHTTP(w, req)
 		} else if strings.HasPrefix(authorization, "Basic ") || strings.HasPrefix(authorization, "basic ") {
 			basicAuth := strings.Split(authorization, " ")[1]
