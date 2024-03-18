a9e245059f668db5b329d206bf236c6155bf51ed
Ping-Lin Chang <ping-lin.chang@instill.tech>
2024-03-18 13:28:28 +0000

    feat(registry): add auth and docker login

--- registry-plugin/main.go
+++ registry-plugin/main.go
@@ -6,7 +6,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"regexp"
 	"strings"
+
+	"google.golang.org/grpc/metadata"
+
+	mgmtPB "github.com/instill-ai/protogen-go/core/mgmt/v1beta"
 )
 
 // pluginName is the plugin name
@@ -24,7 +29,7 @@ func (r registerer) RegisterHandlers(f func(
 	f(string(r), r.registerHandlers)
 }
 
-func (r registerer) registerHandlers(_ context.Context, extra map[string]interface{}, h http.Handler) (http.Handler, error) {
+func (r registerer) registerHandlers(ctx context.Context, extra map[string]interface{}, h http.Handler) (http.Handler, error) {
 
 	config, ok := extra[pluginName].(map[string]interface{})
 	if !ok {
@@ -32,31 +37,122 @@ func (r registerer) registerHandlers(_ context.Context, extra map[string]interfa
 	}
 
 	hostport, _ := config["hostport"].(string)
-	prefix, _ := config["prefix"].(string)
+
+	mgmtPublicClient, _ := InitMgmtPublicServiceClient(ctx, config["mgmt_public_hostport"].(string), "", "")
+	mgmtPrivateClient, _ := InitMgmtPrivateServiceClient(ctx, config["mgmt_private_hostport"].(string), "", "")
 
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 
-		// If the URL path starts with "/v2/" (indicating the first handshake request to confirm registry V2 API),
-		// "/v2/prefix/" (before the registry prefix is applied), or "/prefix/v2/" (after the registry prefix is applied),
+		// If the URL path starts with "/v2/" (exactly /v2/ indicating the first handshake request to confirm registry V2 API),
 		// it means that the request is intended for the Instill Artifact registry. In this case, the traffic is hijacked
 		// and directly relayed to the registry. Otherwise, if the URL path does not match any of these patterns,
 		// the traffic is passed through to the next handler.
-		if req.URL.Path != "/v2/" &&
-			!strings.HasPrefix(req.URL.Path, fmt.Sprintf("/v2%s", prefix)) &&
-			!strings.HasPrefix(req.URL.Path, fmt.Sprintf("%sv2/", prefix)) {
+		if !strings.HasPrefix(req.URL.Path, "/v2/") {
 			h.ServeHTTP(w, req)
 			return
 		}
 
+		// Authenticate the user via docker login
+		var username, password string
+		if req.URL.Path == "/v2/" {
+			username, password, ok = req.BasicAuth()
+			if !ok {
+				// Challenge the user for basic authentication
+				w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+				writeStatusUnauthorized(req, w, "Instill AI user authentication failed")
+				return
+			}
+
+			// Validate the api key and the namespace authorization
+			if !strings.HasPrefix(password, "instill_sk_") {
+				writeStatusUnauthorized(req, w, "Instill AI user authentication failed")
+				return
+			}
+
+			ctx := metadata.AppendToOutgoingContext(ctx, "Authorization", fmt.Sprintf("Bearer %s", password))
+			if resp, err := mgmtPublicClient.ValidateToken(
+				ctx,
+				&mgmtPB.ValidateTokenRequest{},
+			); err == nil {
+				userUID := resp.UserUid
+				// Check if the login username is the same with the user id retrieved from the token validation response
+				if resp, err := mgmtPrivateClient.LookUpUserAdmin(
+					ctx,
+					&mgmtPB.LookUpUserAdminRequest{
+						Permalink: "users/" + userUID,
+					},
+				); err == nil {
+					if resp.User.Id != username {
+						fmt.Println(1)
+						writeStatusUnauthorized(req, w, "Instill AI user authentication failed")
+						return
+					}
+				} else {
+					logger.Error(err.Error())
+					writeStatusInternalError(req, w)
+					return
+				}
+
+				// To this point, if the req.URL.Path is "/v2/", return 200 OK to the client for login success
+				writeStatusOK(req, w)
+				return
+			}
+
+			fmt.Println(2)
+			writeStatusUnauthorized(req, w, "Instill AI user authentication failed")
+			return
+		}
+
+		// Docker image tag format:
+		// [registry]/[namespace]/[repository path]:[image tag]
+		// The namespace is the user uid or the organization uid
+		var namespace string
+		matches := regexp.MustCompile(`/v2/([^/]+).*`).FindStringSubmatch(req.URL.Path)
+		if len(matches) >= 2 {
+			namespace = matches[1]
+		} else {
+			writeStatusUnauthorized(req, w,
+				"Namespace is not found in the image name. Docker registry hosted in Instill Artifact has a format"+
+					"[registry]/[namespace]/[repository path]:[image tag]."+
+					"A namespace can be a user or organization ID.")
+			return
+		}
+
+		// If the username and the namespace is not the same,
+		// check if the namespace is an organisation name where the user has the membership
+		if username != "" && namespace != username {
+			resp, err := mgmtPublicClient.ListUserMemberships(
+				ctx,
+				&mgmtPB.ListUserMembershipsRequest{
+					Parent: fmt.Sprintf("users/%s", username),
+				})
+			if err != nil {
+				fmt.Println(3)
+				writeStatusUnauthorized(req, w, "Instill AI user authentication failed")
+				return
+			}
+			isValid := false
+			for _, membership := range resp.Memberships {
+				if namespace == membership.Organization.Name && membership.State == mgmtPB.MembershipState_MEMBERSHIP_STATE_ACTIVE {
+					isValid = true
+					break
+				}
+			}
+			if !isValid {
+				fmt.Println(4)
+				writeStatusUnauthorized(req, w, "Instill AI user authentication failed")
+				return
+			}
+		}
+
 		req.URL.Scheme = "http"
 		req.URL.Host = hostport
-		req.URL.Path = strings.TrimSuffix(prefix, "/") + strings.Replace(req.URL.Path, prefix, "/", 1)
 		req.RequestURI = ""
 
 		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
 			logger.Error(err.Error())
-			w.WriteHeader(http.StatusInternalServerError)
+			writeStatusInternalError(req, w)
 			return
 		}
 
@@ -66,11 +162,53 @@ func (r registerer) registerHandlers(_ context.Context, extra map[string]interfa
 				w.Header().Add(k, h)
 			}
 		}
+
 		w.WriteHeader(resp.StatusCode)
+		w.Header().Set("Content-Type", "application/json")
 		io.Copy(w, resp.Body)
 		resp.Body.Close()
 
 	}), nil
+}
+
+func writeStatusUnauthorized(req *http.Request, w http.ResponseWriter, error string) {
+	if req.ProtoMajor == 2 && strings.Contains(req.Header.Get("Content-Type"), "application/grpc") {
+		w.Header().Set("Content-Type", "application/grpc")
+		w.Header().Set("Trailer", "Grpc-Status")
+		w.Header().Add("Trailer", "Grpc-Message")
+		w.Header().Set("Grpc-Status", "16")
+		w.Header().Set("Grpc-Message", "Unauthenticated")
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Header().Set("Content-Type", "application/json")
+	}
+	fmt.Fprintln(w, error)
+}
+
+func writeStatusInternalError(req *http.Request, w http.ResponseWriter) {
+	if req.ProtoMajor == 2 && strings.Contains(req.Header.Get("Content-Type"), "application/grpc") {
+		w.Header().Set("Content-Type", "application/grpc")
+		w.Header().Set("Trailer", "Grpc-Status")
+		w.Header().Add("Trailer", "Grpc-Message")
+		w.Header().Set("Grpc-Status", "13")
+		w.Header().Set("Grpc-Message", "INTERNAL")
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "application/json")
+	}
+}
+
+func writeStatusOK(req *http.Request, w http.ResponseWriter) {
+	if req.ProtoMajor == 2 && strings.Contains(req.Header.Get("Content-Type"), "application/grpc") {
+		w.Header().Set("Content-Type", "application/grpc")
+		w.Header().Set("Trailer", "Grpc-Status")
+		w.Header().Add("Trailer", "Grpc-Message")
+		w.Header().Set("Grpc-Status", "0")
+		w.Header().Set("Grpc-Message", "OK")
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+	}
 }
 
 func main() {}
